d6cda8c37babce77e4dbe48b86ea9f0c9b19b0b3
calerc1 <carloscalero14@gmail.com>
2017-04-27 18:57:43 -0400

    made function to print Process objects

--- Process.py
+++ Process.py
@@ -3,7 +3,9 @@ from __future__ import print_function
 class Process:
 	
 	def __init__(self, Name, NumMem, Data):
-		self.name
-		self.numMem
-		self.data = data
-
+		self.name = Name
+		self.numMem = NumMem
+		self.data = Data
+	
+	def __str__(self):
+		return self.name + " " +   self.numMem + " " +  str(self.data)
