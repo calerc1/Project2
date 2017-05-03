48de483459336c2967ce498dc8e3e0ab2883f4db
Carlos <carloscalero14@gmail.com>
2017-05-03 13:53:21 -0400

    merge

--- Process.py
+++ Process.py
@@ -3,7 +3,11 @@ from __future__ import print_function
 class Process:
 	
 	def __init__(self, Name, NumMem, Data):
-		self.name
-		self.numMem
-		self.data = data
+		self.name = Name
+		self.numMem = NumMem
+		self.start = Data[0]
+                self.stop = Data[1]
+
+        def __str__ (self):
+                return self.name + " " + self.numMem + " " + self.start + "-" + self.stop
 
--- project2.py
+++ project2.py
@@ -3,62 +3,50 @@ import sys
 import Process
 
 def initData():
-	data = []
+        data = []
         data.append( ['.', 0, 255] )
-	return data
+        return data
 
 def printData( data ):
-        data = [ ['A', 0, 45], ['.', 46, 255] ]
-	print ( "=" * 32 )
-        current  = 0
-        line = 1
-        for element in data:
-                # final - initial point
-                len = element[2] - element[1]
-                while (current < element[2]):
-                        endLine = line * 32
-                        if ( (current + len) >= endLine ):
-                                print (element[0] * (endLine - current) ),
-                                current = endLine
-                                line += 1
-                        else:
-                                print (element[0] * (len - current) ),
-                                current = endLine
-                                
-                                
-	print ("=" * 32)
-
+        return
+        
 ''' @param name of input file
-	@return a list of objects type Process
+@return a list of objects type Process
 '''
-
 def parseFile(filename):
-	process = []
-	num = 0
-	for line in open(fileName, "r"):
-		temp = (line.strip()).split(" ")
-		if(num == 0):
-			num =temp[0] 	
-		else:
-			print(temp)
-			
+        process = []
+        
+        num = 0
+        for line in open(fileName, "r"):
+                temp = (line.strip()).split(" ")
+                if(num == 0):
+                        num =temp[0] 	
+                else:
+                        print(temp)
 
-	return [process, num]
-	
-	
+
+        return [process, num]
+
+
 
 if __name__ == "__main__":
 
-	data = initData()
+        data = initData()
         print ("Init Memory")
-	printData(data)
-	fileName = sys.argv[1]
-	temp = parseFile(fileName)
-	processes = temp[0]
-	numProcess = temp[1]
-	print(processes)
-	print(numProcess)
-	
-		
-	
+        #printData(data)
+        fileName = sys.argv[1]
+        temp = parseFile(fileName)
+        processes = temp[0]
+        numProcess = temp[1]
+        print(processes)
+        print(numProcess)
+        memList = []
+        for i in processes:
+                for j in i[2]:
+                        memList.append(Process(i[0], i[1], j) )
+        #for i in processes:
+                #print (i)
+
+
+
 
